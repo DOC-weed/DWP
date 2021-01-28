95c84cdd99bcb770b75d9f5d0680b7bb92835d23
Andres <andres@MacBook-Air-de-CEOS-6.local>
2021-01-27 21:53:08 -0600

    ajsjkandaksdkasdlasdjaskdksalkd

--- src/app/components/registro/registro.component.ts
+++ src/app/components/registro/registro.component.ts
@@ -12,37 +12,33 @@ import { UserModel } from 'src/app/core/models/user.model';
 })
 export class RegistroComponent implements OnInit {
  Usuario = new UserModel();
-  //Usuario: User = new User();
-  /*persona = {
-    first_name : "",
-    middle_name : "",
-    last_name : "",
-    phone_number : "",
-    address: {
-        city : "",
-        state : ""
-    },
-    email : "",
-    password : "",
-    password_confirmation : ""
-  }*/
-
   // tslint:disable-next-line: max-line-length
   email_confirmation= new RegExp(/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/);
   pass_confirmation = new RegExp(/^(?=.*\d)(?=.*[\u0021-\u002b\u003c-\u0040])(?=.*[A-Z])(?=.*[a-z])\S{8,16}$/);
-  constructor(public service: ServicesService, public router: Router) { }
+  siteKey: string;
+  miloco_dele_pa_dentro= false;
+  constructor(public service: ServicesService, public router: Router) {
+    this.siteKey = '6LffdDwaAAAAABy1pbhQflEpSfymvai4qxEZREM8';
+   }
 
   ngOnInit(): void {
   }
   registrarUsuario(myForm: NgForm) {
-    
-    this.service.registarUsuario(this.Usuario).then((usuario: any) => {
-      myForm.reset();
-      alert('Registrado correctamente');
-      this.router.navigateByUrl('login');
-    }).catch((err: any) => {
-      alert('Algo salio mal');
-    });
-
+    if(this.miloco_dele_pa_dentro){
+      this.service.registarUsuario(this.Usuario).then((usuario: any) => {
+        myForm.reset();
+        alert('Registrado correctamente');
+        this.router.navigateByUrl('login');
+      }).catch((err: any) => {
+        alert('Algo salio mal');
+      });
+    }else{
+      alert('Capcha invalido');
+    }
+   
 }
+handleSuccess(event){
+  this.miloco_dele_pa_dentro = true;
+}
+
 }
