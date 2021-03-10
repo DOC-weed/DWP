62c50e06d9abf1ba43233f3891b906b0cbdea2c2
JonhyGuy <joulura1609@gmail.com>
2021-03-09 19:19:15 -0600

    Categorys

--- src/app/components/busqueda/busqueda.component.ts
+++ src/app/components/busqueda/busqueda.component.ts
@@ -1,3 +1,4 @@
+import { analyzeAndValidateNgModules } from '@angular/compiler';
 import { Component, OnInit } from '@angular/core';
 import { $ } from 'protractor';
 
@@ -11,62 +12,90 @@ import { ServicesService } from 'src/app/services/services.service';
 })
 export class BusquedaComponent implements OnInit {
 check = false;
-categories : any;
-products:any;
-filterfield= '';
+categories: any;
+products: any;
+filterfield = '';
 busquedaTodo = true;
-busquedaModelo = false;
+busquedaModulo = false;
+productoModulo: any;
 busquedaPaquete = false;
+productoPaquete: any;
 busquedaPlan = false;
+productoPlan: any;
 busquedaServicio = false;
+productoServico: any;
 
-  constructor( public service : ServicesService) { 
-   
-    
-  }
+  constructor( public service: ServicesService) {}
 
   ngOnInit(): void {
-    let licencia ='licencia';
-    let cate = 'plan'
+    this.busquedaTodo = true;
     this.products = this.getAllProducts();
-    this.getEspecificProduct(licencia);
-    this.getAllCategories();
-    this.getEspecificCategory(cate)
-    
-  }
- 
- 
-  getAllCategories(){
-    this.service.getCategories().then((info: any) =>{
-      this.categories= info.data.categories;
-      console.log('categorias');
-      console.log(this.categories);
-    }).catch((err) =>{
-      console.log(err);
-    })
-  }
-  getAllProducts(){
-    this.service.getAllProducts(' ').then((res: any)=>{
-      this.products = res.data.items;
-      console.log('Productos');
-      console.log(this.products);
-    }).catch((err)=>{
-      console.log(err);
-    })
-  }
-  getEspecificProduct(name){
-    this.service.getAllProducts(name).then((res: any)=>{
-      console.log(res.data.items);
-    }).catch((err)=>{
-      console.log(err);
-    })
-  }
-  getEspecificCategory(name){
-    this.service.getEspecificCategory(name).then((res: any)=>{
-      console.log(res.data.items);
-    }).catch((err)=>{
-      console.log(err);
-    })
+    this.getEspecificCategory('');
+    this.categories = this.getAllCategories();
   }
 
+  getAllCategories(){
+    this.service.getCategories().then((info: any) => {
+      this.categories = info.data.categories;
+      // console.log('categorias');
+      // console.log(this.categories);
+    }).catch((err) => {
+      console.log(err);
+    });
+  }
+  getAllProducts(){
+    this.service.getAllProducts(' ').then((res: any) => {
+      this.products = res.data.items;
+      // console.log('Productos');
+      // console.log(this.products);
+    }).catch((err) => {
+      console.log(err);
+    });
+  }
+  getEspecificProduct(name){
+    this.service.getAllProducts(name).then((res: any) => {
+      // console.log(res.data.items);
+    }).catch((err) => {
+      console.log(err);
+    });
+  }
+
+  async getEspecificCategory(name){
+    let products: any;
+    await this.service.getEspecificCategory(name).then((res: any) => {
+      console.log(res.data.items);
+      products = res.data.items;
+      // console.log(this.NuevosProductos);
+    }).catch((err) => {
+      console.log(err);
+    });
+    return products;
+  }
+
+   async sortByCategory() {
+    if(this.busquedaModulo === false && this.busquedaPlan === false && this.busquedaPaquete === false && this.busquedaServicio === false ) {
+      this.busquedaTodo = true;
+      this.products = this.getAllProducts();
+    }
+    if (this.busquedaModulo) {
+      this.busquedaTodo = false;
+      const res = await this.getEspecificCategory('Módulo');
+      this.productoModulo = res;
+    }
+    if(this.busquedaPlan){
+      this.busquedaTodo = false;
+      const res = await this.getEspecificCategory('Plan');
+      this.productoPlan = res;
+    }
+    if(this.busquedaPaquete) {
+      this.busquedaTodo = false;
+      const res = await this.getEspecificCategory('Paquete');
+      this.productoPaquete = res;
+    }
+    if(this.busquedaServicio){
+      this.busquedaTodo = false;
+      const res = await this.getEspecificCategory('Servicio');
+      this.productoServico = res;
+    }
+  }
 }
